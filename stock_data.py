7334c9a8182681955c98bd3f4ec3417b1b93593d
mhendal <qualitymehal@gmail.com>
2021-07-02 04:57:12 +0300

    ferist commit

---
+++ stock_data.py
@@ -0,0 +1,4 @@
+    df = investpy.get_stock_historical_data(stock='AAPL',
+                                        country='United States',
+                                        from_date='01/01/2010',
+                                        to_date='01/01/2020')
